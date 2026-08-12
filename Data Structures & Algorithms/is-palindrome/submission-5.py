9e8cb1da30790895b2f48046dbba538831042176
Takkallapally Sai Shodhan Rao <64457083+tssrao@users.noreply.github.com>
2026-08-12 09:54:46 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # Time Complexity and Space Complexity: O(n)
+        # Two Pointer: O(1) Space Complexity
+        valid_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+        reversed_str = []
+        for i in range(len(s)-1,-1,-1):
+            if s[i].lower() in valid_chars:
+                reversed_str.append(s[i].lower())
+        # print(reversed_str)
+
+        actual_str = []
+        for i in s:
+            if i.lower() in valid_chars:
+                actual_str.append(i.lower())
+        # print(actual_str)
+
+        if actual_str == reversed_str:
+            return True
+        else:
+            return False
+        
